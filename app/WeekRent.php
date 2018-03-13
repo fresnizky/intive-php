7d5ac6b5f5e527646997fab7f6c10748f0917edb
Federico Resnizky <fresnizky@yahoo.com.ar>
2018-03-13 10:39:55 -0300

    Added base classes and migrations

--- app/WeekRent.php
+++ app/WeekRent.php
@@ -7,7 +7,7 @@ class WeekRent extends Rent
     public function __construct(array $attributes = [])
     {
         $this->type = Rent::WEEK_TYPE;
-        $this->price_cents = 500;
+        $this->price_cents = 6000;
         $this->max_duration = 4;
 
         parent::__construct($attributes);
